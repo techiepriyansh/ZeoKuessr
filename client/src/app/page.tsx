3f255dc6485223890635bcaf2743c542e5bac95b
Mayank Mittal <msquare.pvt21@gmail.com>
2024-09-22 06:00:22 +0800

    Client

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,7 +7,7 @@ import Map from "@/components/Map";
 export default function Home() {
   const { connect, address, isConnected, disconnect } = useOasis();
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen bg-black">
       <div className="h-1/6 flex flex-row justify-center items-center">
         <WalletConnect />
       </div>
