29e9f96cade9c847cfbde93eb6fb9145e399bcbd
Priyansh Rathi <techiepriyansh@gmail.com>
2024-09-21 14:18:02 +0800

    Interact with offchain

--- backend/contracts/MessageBox.sol
+++ backend/contracts/MessageBox.sol
@@ -3,21 +3,59 @@ pragma solidity ^0.8.0;
 
 import "@oasisprotocol/sapphire-contracts/contracts/Sapphire.sol";
 
+uint256 constant OP_GET_GEO_LOCATION_IMAGE = 1; // getGeoLocationImage(locationSeed bytes)
+uint256 constant OP_CALC_POOL_PARTITION = 2; // calcPoolPartition(gameId uint256, poolAmount uint256, locationSeed bytes, numUser uint256, userGuesses bytes[])
+
+struct OffchainTx {
+	uint256 id;
+	uint256 op;
+	bytes[] args;
+}
+
 contract MessageBox {
     string private _message;
     address public author;
 
-	bytes public pk;
-	bytes private sk;
+	mapping(uint256 => OffchainTx) private offchainTxs;
 
-	constructor() {
-		bytes memory seed = Sapphire.randomBytes(32, "");
-		Sapphire.SigningAlg alg = Sapphire.SigningAlg.Secp256k1PrehashedKeccak256;
-		(pk, sk) = Sapphire.generateSigningKeyPair(alg, seed); // Public/Secret key
+	uint256 private txIdStart = 1;
+	uint256 private txIdEnd = 1;
+
+	event GeoLocationImage(uint256 indexed txId, string imageId);
+
+	function callOffchain(uint256 op, bytes[] memory args) internal returns (uint256) {
+		uint256 txId = txIdEnd++;
+		OffchainTx memory offchainTx = OffchainTx(txId, op, args);
+		offchainTxs[txId] = offchainTx;
+		return txId;
 	}
 
-	function getPublicKey() external view returns (bytes memory) {
-		return pk;
+	function getPendingOffchainTxs() external view returns (OffchainTx[] memory) {
+		// TODO: access restricted to the ROFL
+		OffchainTx[] memory pendingOffchainTxs = new OffchainTx[](txIdEnd - txIdStart);
+		for (uint256 i = txIdStart; i < txIdEnd; i++) {
+			pendingOffchainTxs[i - txIdStart] = offchainTxs[i];
+		}
+		return pendingOffchainTxs;
+	}
+
+	function sendResultFromOffchain(uint256 txId, bytes[] memory result) external {
+		// TODO: access restricted to the ROFL
+		require(txId == txIdStart, "txId does not match the first pending tx");
+		OffchainTx memory offchainTx = offchainTxs[txId];
+		if (offchainTx.op == OP_GET_GEO_LOCATION_IMAGE) {
+			// (imageId string)
+			emit GeoLocationImage(txId, string(result[0]));
+		} else if (offchainTx.op == OP_CALC_POOL_PARTITION) {
+			// (gameId uint256, uint256[] partitionAmounts)
+		}
+		delete offchainTxs[txId];
+		txIdStart++;
+	}
+
+	constructor(string memory in_message) {
+		_message = in_message;
+		author = msg.sender;		
 	}
 
     function setMessage(string calldata in_message) external {
