fffb84175f03899fe23096ed8b2eb56e3d752d90
Anshul Singh <anshulspidy@gmail.com>
2024-09-22 03:33:34 +0800

    smart contr push logic

--- rofl-oracle/src/main.rs
+++ rofl-oracle/src/main.rs
@@ -1,6 +1,5 @@
 use oasis_runtime_sdk::{crypto::signature::secp256k1::PublicKey, modules::rofl::app::prelude::*, types::address::SignatureAddressSpec};
-use ethers::prelude::*;
-use ethabi;
+use ethabi::{self, ethereum_types::U256};
 /// Address where the oracle contract is deployed.
 // #region oracle-contract-address
 const ORACLE_CONTRACT_ADDRESS: &str = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // TODO: Replace with your contract address.
@@ -78,7 +77,7 @@ impl OracleApp {
             .parse::<f64>()?;
             let price = (price * 1_000_000.0) as u128;
         
-            println!("SHIKA2!");
+            println!("SHIKA3!");
             Ok(price)
         }).await??;
 
@@ -116,24 +115,60 @@ impl OracleApp {
         // // Submit observation on chain.env
         // env.client().sign_and_submit_tx(env.signer(), tx).await?;
 
-        let sdk_pub_key = PublicKey::from_bytes(env.signer().public_key().as_bytes()).unwrap();
-        let res = env.client().query(
-            0,
-            "evm.SimulateCall",
-            module_evm::types::SimulateCallQuery {
-                gas_price: 10.into(),
-                gas_limit: 100_000,
-                caller: module_evm::derive_caller::from_sigspec(&SignatureAddressSpec::Secp256k1Eth(sdk_pub_key)).unwrap(),
-                address: None,
+        // let sdk_pub_key = PublicKey::from_bytes(env.signer().public_key().as_bytes()).unwrap();
+        // let res = env.client().query(
+        //     0,
+        //     "evm.SimulateCall",
+        //     module_evm::types::SimulateCallQuery {
+        //         gas_price: 10.into(),
+        //         gas_limit: 100_000,
+        //         caller: module_evm::derive_caller::from_sigspec(&SignatureAddressSpec::Secp256k1Eth(sdk_pub_key)).unwrap(),
+        //         address: None,
+        //         value: 0.into(),
+        //         data: [
+        //           ethabi::short_signature("getFirstPendingOffchainTx", &[]).to_vec(),
+        //           ethabi::encode(&[]),
+        //         ].concat(),
+        //     },
+        // ).await?;
+
+        
+        let mut tx_id: U256 = U256::from(12345);
+        let result: Vec<Vec<u8>> = vec![
+            vec![0x01, 0x02, 0x03],  // First byte array
+            vec![0x04, 0x05],        // Second byte array
+            ];
+            
+        println!("ENGINIGGER PUSH 1");
+        
+        let mut tx = self.new_transaction(
+            "evm.Call",
+            module_evm::types::Call {
+                address: ORACLE_CONTRACT_ADDRESS.parse().unwrap(),
                 value: 0.into(),
                 data: [
-                  ethabi::short_signature("getFirstPendingOffchainTx", &[]).to_vec(),
-                  ethabi::encode(&[]),
-                ].concat(),
-            },
-        ).await?;
-
-        println!("ENGINIGGER PULL");
+                    // Short signature for sendResultFromOffchain(uint256, bytes[])
+                    ethabi::short_signature("sendResultFromOffchain", &[ethabi::ParamType::Uint(256), ethabi::ParamType::Array(Box::new(ethabi::ParamType::Bytes))])
+                    .to_vec(),
+                    
+                    // Encode the txId and result parameters
+                    ethabi::encode(&[
+                        ethabi::Token::Uint(tx_id.into()), // txId as a 256-bit uint
+                        ethabi::Token::Array(result.iter().map(|r| ethabi::Token::Bytes(r.clone())).collect()), // result as an array of bytes
+                        ]),
+                        ]
+                        .concat(),
+                    },
+                );
+        
+        println!("ENGINIGGER PUSH 2");
+        
+        tx.set_fee_gas(200_000);
+                
+        // Submit observation on chain.
+        env.client().sign_and_submit_tx(env.signer(), tx).await?;
+        
+        println!("ENGINIGGER PUSH 3");
 
         Ok(())
     }
