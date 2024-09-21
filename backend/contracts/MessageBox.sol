d9684b1851818a99717609d1d513fda67d783dc1
Priyansh Rathi <techiepriyansh@gmail.com>
2024-09-21 10:12:09 +0800

    Setup encryption keypair

--- backend/contracts/MessageBox.sol
+++ backend/contracts/MessageBox.sol
@@ -1,9 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@oasisprotocol/sapphire-contracts/contracts/Sapphire.sol";
+
 contract MessageBox {
     string private _message;
     address public author;
+
+	bytes public pk;
+	bytes private sk;
+
+	constructor() {
+		bytes memory seed = Sapphire.randomBytes(32, "");
+		Sapphire.SigningAlg alg = Sapphire.SigningAlg.Secp256k1PrehashedKeccak256;
+		(pk, sk) = Sapphire.generateSigningKeyPair(alg, seed); // Public/Secret key
+	}
+
+	function getPublicKey() external view returns (bytes memory) {
+		return pk;
+	}
 
     function setMessage(string calldata in_message) external {
         _message = in_message;
